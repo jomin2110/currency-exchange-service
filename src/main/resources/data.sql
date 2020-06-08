600fac68aa3732fc091338bef538b91b0558eb74
Jose James <j>
2020-06-08 11:43:41 +0100

    initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+Insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10001, 'USD', 'GBP', 0.80, 0);
+Insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10002, 'USD', 'EUR', 0.92, 0);
+Insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10003, 'USD', 'AUD', 1.54, 0);
